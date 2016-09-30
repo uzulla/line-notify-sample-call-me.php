5e1263276c78f61c6eac161677aa727784f306a8
uzulla <uzulla@himitsukichi.com>
2016-10-01 00:33:55 +0900

    あまりにもひどかったのでそれっぽくした。

--- callback.php
+++ callback.php
@@ -1,3 +1,10 @@
+<?php
+include __DIR__."/settings.php";
+include __DIR__."/LineNotifySimpleLib.php";
+
+$line_notify = new \Uzulla\Net\LineNotifySimpleLib(LINE_NOTIFY_CLIENT_ID, LINE_NOTIFY_CLIENT_SECRET, CALLBACK_URL);
+$access_token = $line_notify->requestAccessToken($_GET);
+?>
 <!doctype html>
 <html lang="en">
 <head>
@@ -7,80 +14,19 @@
 <body>
 
 <?php
-include __DIR__."/settings.php";
-session_start();
-
-// セキュリティの為にstateがセッションと一致するか確認します
-if(!isset($_SESSION['state_random_str']) || $_GET['state'] !== $_SESSION['state_random_str'] ){
-    echo "セッションが不正です、最初からやりなおしてください。";
-    echo "<a href='/'>トップへもどる</a>";
-    $_SESSION = [];
-    exit;
-}
-
-// 確認したので、セッションを必ず消去します
-$_SESSION = [];
-
-// APIへのPOSTデータ用意
-$post_content = http_build_query([
-    'code' => (string)$_GET['code'],
-    'redirect_uri' => CALLBACK_URL,
-    'client_id' => LINE_NOTIFY_CLIENT_ID,
-    'client_secret' => LINE_NOTIFY_CLIENT_SECRET,
-    'grant_type' => 'authorization_code',
-], "", "&");
-
-$header = [
-    "Content-Type: application/x-www-form-urlencoded",
-    "Content-Length: ".strlen($post_content)
-];
-
-$context = stream_context_create([
-    "http" => [
-        "method"  => "POST",
-        "header"  => $header,
-        "content" => $post_content
-    ]
-]);
-
-// 送信
-$response_json_str = file_get_contents('https://notify-bot.line.me/oauth/token', false, $context);
-
-// レスポンスされた結果を確認
-$response = json_decode($response_json_str, true);
-
-// Parseできているか、statusが200か、access_tokenが有るか確認する
-if(!isset($response['status']) || $response['status'] != 200 || !isset($response['access_token'])){
-    echo "<pre>";
-    echo "\n<h1>認証失敗</h1>\n";
-
-    // デバッグするなら、以下を戻すと良いでしょう
-//    echo "\nSESSION vars\n";
-//    echo htmlspecialchars(print_r($_SESSION,1), ENT_QUOTES);
-//    echo "\nGET params\n";
-//    echo htmlspecialchars(print_r($_GET,1), ENT_QUOTES);
-//    echo "\nResponse Header\n";
-//    echo htmlspecialchars(print_r($http_response_header,1), ENT_QUOTES);
-//    echo "\nParsed response\n";
-//    echo htmlspecialchars(print_r($response,1), ENT_QUOTES);
-//    echo "\nResponse JSON\n";
-//    echo htmlspecialchars(print_r($response_json_str,1), ENT_QUOTES);
-
-    echo "<p><a href='/'>トップへもどる</a></p>";
-
-}else if(preg_match('/[^a-zA-Z0-9]/u', $response['access_token'])){
-    echo "<h1>エラー</h1>";
-    echo "<p>なぜかaccess_tokenとして返された値が怪しいかもしれません</p>";
-    htmlspecialchars(print_r($response['access_token'],1), ENT_QUOTES);
-
+if($access_token===false){
+    echo "<h1>認証失敗</h1>";
+    echo "<p>以下はデバッグ情報です（通常表示する必要はありません）</p>";
+    echo "<textarea>";
+    echo htmlspecialchars(print_r($line_notify->getLastError(),1), ENT_QUOTES);
+    echo "</textarea>";
 }else{
     echo "<h1>認証成功</h1>";
-    echo "<p>access_token: {$response['access_token']}</p>";
-    echo "<p>以下をsettings.phpに追記してください</p>";
-    echo "<textarea style='width:100%;'>define('ACCESS_TOKEN', '{$response['access_token']}');</textarea>";
-    echo "<p><a href='/'>トップへもどる</a></p>";
+    echo "<p>access_token <input type='text' width='400px' value='".htmlspecialchars($access_token, ENT_QUOTES)."'></p>";
+    echo "<p>上記access_tokenをsettings.phpに設定してください</p>";
 }
 ?>
 
+<p><a href="/">トップに戻る</a></p>
 </body>
 </html>
--- settings.sample.php
+++ settings.sample.php
@@ -5,4 +5,4 @@ date_default_timezone_set('Asia/Tokyo');
 define('LINE_NOTIFY_CLIENT_ID','SET_YOUR_CLIENT_ID');
 define('LINE_NOTIFY_CLIENT_SECRET','SET_YOUR_CLIENT_SECRET');
 define('CALLBACK_URL','http://example.jp/SET_YOUR_URL');
-// define('ACCESS_TOKEN', 'start_authorize.phpで取得します');
+define('ACCESS_TOKEN', 'ここに取得したTOKENを設定します');
